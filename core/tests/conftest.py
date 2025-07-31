2781b8114eb62a320c3fa032362c722b77f2687f
REZASH600 <rezasharafdini973@gmail.com>
2025-07-31 20:13:52 +0330

    test(fixtures): enable pg_trgm extension for PostgreSQL in tests
    Added a pytest fixture that creates the pg_trgm extension automatically
    before running tests to fix similarity function errors. This ensures
    that trigram-based filters and searches work correctly in test databa

--- core/tests/conftest.py
+++ core/tests/conftest.py
@@ -2,3 +2,4 @@ from tests.fixtures.user import *
 from tests.fixtures.profile import *
 from tests.fixtures.restaurant import *
 from tests.fixtures.request import *
+from tests.fixtures.db_extensions import *
